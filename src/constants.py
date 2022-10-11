cce7bf8a2d8b3544ec6bafcd31f76cf657419877
Camilo Barros <Camilo.Barros@Millicom.com>
2022-10-11 16:23:53 -0300

    added sensitive data to .env file

---
+++ src/constants.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+FACETEC_DASHBOARD = os.getenv('FACETEC_DASHBOARD')
+USERNAME = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD')
--- src/facetec.py
+++ src/facetec.py
@@ -8,8 +8,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
+from src.constants import FACETEC_DASHBOARD, USERNAME, PASSWORD
+
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get('https://alb-dev-alb-face-dash-pa-433088894.us-east-1.elb.amazonaws.com/')
+driver.get(FACETEC_DASHBOARD)
 button = driver.find_element(By.ID, 'details-button')
 button.click()
 proceed = driver.find_element(By.ID, 'proceed-link')
@@ -17,12 +19,12 @@ proceed.click()
 try:
     form = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, "visible-md")))
     userInput = form.find_element(By.ID, 'signInFormUsername')
-    userInput.send_keys('camilo.barros')
+    userInput.send_keys(USERNAME)
     passwordInput = form.find_element(By.ID, 'signInFormPassword')
-    passwordInput.send_keys('Facetec.123')
+    passwordInput.send_keys(PASSWORD)
     passwordInput.send_keys(Keys.RETURN)
 
-    driver.get('https://alb-dev-alb-face-dash-pa-433088894.us-east-1.elb.amazonaws.com/photo-id-matches')
+    driver.get(f'{FACETEC_DASHBOARD}photo-id-matches')
 
     time.sleep(10)
 finally:
